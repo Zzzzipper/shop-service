6dc3d6193112c3d62ba4ad180463f2fec823e4dd
Eduard Pozdniakov <e.pozdniakov@mapcard.pro>
2022-12-06 19:11:36 +0300

    M2-36: Up migration to 3

--- app/merchant/helpers.go
+++ app/merchant/helpers.go
@@ -26,7 +26,7 @@ var fs embed.FS
 
 // version defines the current migration version. This ensures the app
 // is always compatible with the version of the database.
-const version = 2
+const version = 3
 
 // Migrate migrates the Postgres schema to the current version.
 func validateSchema(db *sql.DB, scheme string) error {
